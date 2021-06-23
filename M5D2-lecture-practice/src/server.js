add31edddc1d745f90d58ff26f9584af055b949d
Helena Nel <mailhedri@gmail.com>
2021-06-23 11:56:20 +0200

    lecture code (following)

--- M5D2-lecture-practice/src/server.js
+++ M5D2-lecture-practice/src/server.js
@@ -1,17 +1,40 @@
 import express from "express";
 import listEndpoints from "express-list-endpoints";
+import cors from "cors";
 
 import usersRouter from "./services/users/index.js";
 import booksRouter from "./services/books/index.js";
+
+const port = 3001;
+
 const server = express();
-const port = 3333;
 
-// <------------------------ ENDPOINTS
+// ************************** MIDDLEWARES **************************
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(`Request --> ${req.method} ${req.url} -- ${new Date()}`);
+  next(); // mandatory to give the control to what is happening next
+};
+
+const loggerMiddleware2 = (req, res, next) => {
+  console.log(`Request --> ${req.method} ${req.url} -- ${new Date()}`);
+  next(); // mandatory to give the control to what is happening next
+};
+
+server.use(cors()); // GLOBAL MIDDLEWARE
+server.use(express.json()); // GLOBAL MIDDLEWARE
+server.use(loggerMiddleware); // GLOBAL MIDDLEWARE
+
+// ************************* ENDPOINTS ***************************
+
 server.use("/users", usersRouter);
-server.use("/books", booksRouter);
+server.use("/books", loggerMiddleware2, booksRouter); // loggerMiddleware2 is a ROUTER LEVEL MIDDLEWARE
 
+// *************************** ERROR HANDLERS ***************************
+
+server.user(catchErrorMiddleware);
 console.table(listEndpoints(server));
 
 server.listen(port, () => {
-  console.log(`You have awakend a portal on port ${port} enter if you dare`);
+  console.log(`Server is running on port ${port}`);
 });
--- M5D2-lecture-practice/src/services/books/index.js
+++ M5D2-lecture-practice/src/services/books/index.js
@@ -1,4 +1,94 @@
-import express from "express";
+import express from 'express'
+import fs from 'fs'
+import {fileURLToPath} from 'url'
+import {dirname, join} from 'path'
+import uniqid from 'uniqid'
 
-const booksRouter = express.Router();
-export default booksRouter;
+const booksRouter = express.Router()
+
+// const booksJsonPath = join(dirname(fileURLToPath(import.meta.url)), "../../../node_modules/books.json")
+
+const booksJsonPath = join(dirname(fileURLToPath(import.meta.url)), "books.json")
+
+const getbooksArray = () => {
+  const content = fs.readFileSync(booksJsonPath)
+  return JSON.parse(content)
+}
+
+const writebooks = (content) => fs.writeFileSync(booksJsonPath, JSON.stringify(content))
+
+// 1.
+booksRouter.get("/", (req, res) => {
+  const books = getbooksArray()
+
+  console.log(req.query)
+  if(req.query && req.query.title) {
+    const filteredBooks = books.filter(book => book.hasOwnProperty("title") && book.title === req.query.title)
+    res.send(filteredBooks)
+  } else {
+
+    res.send(books)
+  }
+})
+
+// 2.
+booksRouter.get("/:userId", (req, res) => {
+  const books = getbooksArray()
+
+  const user = books.find(u => u._id === req.params.userId)
+
+  res.send(user)
+})
+
+// 3.
+booksRouter.post("/", (req, res) => {
+  const newUser = {...req.body, _id: uniqid(), createdAt: new Date()}
+
+  const books = getbooksArray()
+
+  books.push(newUser)
+
+  writebooks(books)
+  res.status(201).send({_id: newUser._id})
+})
+
+// 4.
+booksRouter.put("/:userId", (req, res) => {
+  const books = getbooksArray()
+
+  const remainingbooks = books.filter(user => user._id !==req.params.userId)
+
+  const updatedUser = {...req.body, _id: req.params.userId}
+
+  remainingbooks.push(updatedUser)
+
+  writebooks(remainingbooks)
+
+  res.send(updatedUser)
+})
+
+// 5.
+booksRouter.delete("/:userId", (req, res) => {
+
+  const books = getbooksArray()
+  const remainingbooks = books.filter(user => user._id !==req.params.userId)
+
+  writebooks(remainingbooks)
+
+  res.status(204).send()
+})
+
+export default booksRouter
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Loading complete
